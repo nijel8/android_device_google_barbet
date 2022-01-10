ec1b71e661ccd1018b125c0720c55cc6faec3b58
nijel8 <nijel8@gmail.com>
2022-01-10 13:36:15 -0500

    touchscreen: load vendor modules when twrp is installed on boot partition
    Change-Id: Idb8992bb475711a8902627f72a0262065fb39414

--- recovery/root/system/bin/touchdriver.sh
+++ recovery/root/system/bin/touchdriver.sh
@@ -1,4 +1,20 @@
 #!/system/bin/sh
 
-insmod /vendor/lib/modules/1.1/ftm5.ko-redfin
-insmod /vendor/lib/modules/1.1/sec_touch.ko-redfin
+boot_reason=$(getprop ro.boot.bootreason | cut -d "," -F2)
+
+log -t "touchdriver" "boot_reason: $boot_reason"
+
+if [[ $boot_reason == "bootloader" ]]
+then
+    log -t "touchdriver" "loading ramdisk touchdriver"
+    insmod /vendor/lib/modules/1.1/ftm5.ko-redfin
+    insmod /vendor/lib/modules/1.1/sec_touch.ko-redfin
+    log -t "touchdriver" "loaded ramdisk touchdriver"
+else
+    log -t "touchdriver" "loading vendor touchdriver"
+    twrp mount vendor
+    modprobe -d /vendor/lib/modules ftm5.ko
+    modprobe -d /vendor/lib/modules sec_touch
+    twrp unmount vendor
+    log -t "touchdriver" "loaded vendor touchdriver"
+fi
